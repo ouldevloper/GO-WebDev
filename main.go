35cfa9a16a76cb9b251047ac0795c4761e9b6ac3
user <aitbaha48@gmail.com>
2023-12-28 04:59:56 +0300

    -

--- main.go
+++ main.go
@@ -12,10 +12,13 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello Go Web Dev")
+	fmt.Fprint(w, "<p>Hello Go Web Dev</p></p>")
+	fmt.Fprint(w, "<p>Hello Go Web Dev</p></p>")
+	fmt.Fprint(w, "<p>Hello Go Web Dev</p></p>")
+	fmt.Fprint(w, "<p>Hello Go Web Dev</p></p>")
 }
 func main() {
-	fmt.Printf("Server SStarted on 3000")
+	fmt.Printf("Server SStarted on http://127.0.0.1:3000")
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("127.0.0.1:3000", nil)
 }
